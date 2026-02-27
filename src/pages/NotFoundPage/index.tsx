4fd837d2bf5b949be98ee61722f79eb59d720b40
Oleksandr <shkutko93@gmail.com>
2026-02-27 16:19:16 +0200

    Add the Home button to the 404 page.

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,10 +1,17 @@
 import * as Image from '../../images';
 import CenteredContainer from '../../components/Container';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const PageNotFound = () => {
   return (
     <CenteredContainer>
       <Image.PageNotFound size={400} />
+      <Link to='/'>
+        <Button variant='outlined' sx={{ mt: 2 }}>
+          Home
+        </Button>
+      </Link>
     </CenteredContainer>
   );
 };
